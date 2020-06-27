dddcf6ac1aac21b498f8effc50c0779c539c5863
Zmy <s1124yy@gmail.com>
2020-06-27 21:51:28 +0800

    output

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   singleQuote: true,
   trailingComma: 'all',
+  proseWrap: 'never',
+  arrowParens: 'avoid',
+  printWidth: 100,
 };
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: 'this_[name].js',
+  },
+  mode: 'development',
+};
